ecc6837aa82e4fe7c82bbecd6850c999bfeac7ab
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2021-10-14 00:55:48 +0700

    update test case

--- __tests__/solution.test.ts
+++ __tests__/solution.test.ts
@@ -16,6 +16,26 @@ describe("testing the balance bracket '(' and ')'", () => {
   });
 });
 
+describe("testing the balance curly bracket '{' and '}'", () => {
+  const isBracketBalance = new Solution("{", "}");
+
+  test("should return true when curly bracket is balance", () => {
+    expect(isBracketBalance.solve("{}")).toBe(true);
+    expect(isBracketBalance.solve("{}{}{{}}")).toBe(true);
+    expect(isBracketBalance.solve("{{}}")).toBe(true);
+  });
+
+  test("should return false when curly bracket is not balance", () => {
+    expect(isBracketBalance.solve("{")).toBe(false);
+    expect(isBracketBalance.solve("{}{")).toBe(false);
+  });
+
+  test("should return false when given string is not contains curly brace only", () => {
+    expect(isBracketBalance.solve("{pemuda}")).toBe(false);
+    expect(isBracketBalance.solve("{}{indonesia}")).toBe(false);
+  });
+});
+
 describe("testing the error opening and closing tag", () => {
 
   test("should return error when the open tag not char", () => {
